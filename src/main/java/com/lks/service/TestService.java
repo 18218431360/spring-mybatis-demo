011c5eddceec8a3e1ce18b00e1acde6bc6967d72
18218431360 <940651388@qq.com>
2020-12-10 22:25:38 +0800

    添加一个测试类12

--- src/main/java/com/lks/service/TestService.java
+++ src/main/java/com/lks/service/TestService.java
@@ -4,6 +4,7 @@ public class TestService {
 
     public static void main(String[] args) {
         String test = "test";
-        System.out.println(test);
+        String testService = "testService";
+        System.out.println(testService);
     }
 }
