7ec5b548a1811cdd5530a4b2376d395ab11c86c9
18218431360 <940651388@qq.com>
2020-12-20 16:50:03 +0800

    修改测试类的List集合

--- src/main/java/com/lks/service/TestService.java
+++ src/main/java/com/lks/service/TestService.java
@@ -11,19 +11,10 @@ public class TestService {
         String test = "test";
         String testService = "123";
         List<Integer> arrayList = new ArrayList<>();
-        arrayList.add(12);
-        arrayList.add(22);
-        arrayList.add(32);
-        arrayList.add(42);
-        arrayList.add(52);
         arrayList.forEach(item->{
             System.out.println(item);
         });
         HashMap<Object, Object> hashMap = new HashMap<>();
-        hashMap.put("1", 1);
-        hashMap.put("2", 2);
-        hashMap.put("3", 2);
-        hashMap.put("4", 2);
         hashMap.forEach((k,v)->{
             System.out.println(k);
             System.out.println(v);
