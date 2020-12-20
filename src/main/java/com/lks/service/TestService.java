e6b322fdba23420c04a28928a2d40e8716efe37e
18218431360 <940651388@qq.com>
2020-12-20 15:05:28 +0800

    修改测试类的hashMap集合

--- src/main/java/com/lks/service/TestService.java
+++ src/main/java/com/lks/service/TestService.java
@@ -1,10 +1,22 @@
 package com.lks.service;
 
+import java.util.HashMap;
+
 public class TestService {
 
     public static void main(String[] args) {
         String test = "test";
         String testService = "123";
+        HashMap<Object, Object> hashMap = new HashMap<>();
+        hashMap.put(1, 1);
+        hashMap.put(2, 2);
+        hashMap.put(3, 3);
+        hashMap.put(4, 4);
+        hashMap.put(5, 5);
+        hashMap.forEach((k,v)->{
+            System.out.println(k);
+            System.out.println(v);
+        });
         System.out.println(testService);
     }
 }
