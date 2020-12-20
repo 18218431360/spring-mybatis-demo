1a8f966700606771909046c522de70d40bc08063
18218431360 <940651388@qq.com>
2020-12-20 14:57:18 +0800

    修改testService,添加list遍历

--- src/main/java/com/lks/service/TestService.java
+++ src/main/java/com/lks/service/TestService.java
@@ -5,6 +5,14 @@ public class TestService {
     public static void main(String[] args) {
         String test = "test";
         String testService = "123";
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.ForEach(item->{
+            System.out.println(item);
+        })
         System.out.println(testService);
     }
 }
