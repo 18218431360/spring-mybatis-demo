5e92d4f8b5818f1547f77ac22e9ea50b8fe6990a
18218431360 <940651388@qq.com>
2020-12-20 15:08:41 +0800

    修改测试类的hashMap集合

--- src/main/java/com/lks/service/TestService.java
+++ src/main/java/com/lks/service/TestService.java
@@ -1,8 +1,5 @@
 package com.lks.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import java.util.HashMap;
 
 public class TestService {
@@ -10,20 +7,14 @@ public class TestService {
     public static void main(String[] args) {
         String test = "test";
         String testService = "123";
-        List<Integer> arrayList = new ArrayList<>();
-        arrayList.add(1);
-        arrayList.add(2);
-        arrayList.add(3);
-        arrayList.add(4);
-        arrayList.forEach(item->{
-            System.out.println(item);
-        });
         HashMap<Object, Object> hashMap = new HashMap<>();
         hashMap.put(1, 1);
         hashMap.put(2, 2);
         hashMap.put(3, 3);
         hashMap.put(4, 4);
         hashMap.put(5, 5);
+        hashMap.put(6, 6);
+        hashMap.put(7, 7);
         hashMap.forEach((k,v)->{
             System.out.println(k);
             System.out.println(v);
