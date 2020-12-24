0c33fad76ad2b2f2612f500115e37a1572d9f129
18218431360 <940651388@qq.com>
2020-12-24 22:46:49 +0800

    修改测试类..

--- src/main/java/com/lks/service/TestGitOperation.java
+++ src/main/java/com/lks/service/TestGitOperation.java
@@ -4,7 +4,9 @@ public class TestGitOperation {
     public static void main(String[] args) {
         String test = "123";
         String test1 = "234";
+        String adas = "dadsa";
         String test2 = "345345";
+        String qwe = "asad";
         System.out.println();
     }
 }
