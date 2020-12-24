5a2ff69f6aa463dad8190fe48e12d6f12c5f8411
18218431360 <30297477+18218431360@users.noreply.github.com>
2020-12-24 22:35:00 +0800

    Update TestGitOperation.java
    修改测试类

--- src/main/java/com/lks/service/TestGitOperation.java
+++ src/main/java/com/lks/service/TestGitOperation.java
@@ -3,7 +3,8 @@ package com.lks.service;
 public class TestGitOperation {
     public static void main(String[] args) {
         String test = "123";
-        String test1 = "234";
+        String test1 = "0977";
+        String test2 = "5675";
         System.out.println();
     }
 }
