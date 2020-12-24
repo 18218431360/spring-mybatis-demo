81f978e728ae8b72260be640c31458c8d94451a6
18218431360 <30297477+18218431360@users.noreply.github.com>
2020-12-24 22:02:47 +0800

    Update TestService.java
    修改test测试类

--- src/main/java/com/lks/service/TestService.java
+++ src/main/java/com/lks/service/TestService.java
@@ -5,6 +5,7 @@ public class TestService {
     public static void main(String[] args) {
         String test = "test";
         String testService = "testService33123123412";
+        String test = "lks";
         System.out.println(testService);
     }
 }
